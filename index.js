2db78c969e0d71a410ba9d2bcac1ef219e1b8907
Kostja P <kpavlovsky@users.noreply.github.com>
2023-07-29 21:05:29 +0200

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = 3000; // Change this to the desired port number
+const port = process.env.PORT || 3000;
 
 // Database connection
 const pool = new Pool({
